578352ebdcde1be4bc8a4fc95992593a1c1d6d78
Marcos Costa Pinheiro <115197895+Marcos-Costa-Pinheiro@users.noreply.github.com>
2022-12-13 19:57:29 -0300

    exercicios sobre condições

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`Sua velocidade é de ${vel}km/h`)
+if(vel > 60.5){
+    console.log('Limite ultrapassado, MULTADO!')
+}
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'BR'
+console.log(`Vivendo em ${pais}`)
+if(pais != 'BR'){
+    console.log('Você é estrangeiro')
+}else{
+    console.log('Você é brasileiro')
+}
