08d99c2122971c18999cb5b2f6b7f29ae7616196
Marcos Costa Pinheiro <115197895+Marcos-Costa-Pinheiro@users.noreply.github.com>
2023-02-09 18:35:52 -0300

    exercicio 6 e 7

---
+++ udemy/exercicios/ex007/script.js
@@ -0,0 +1,9 @@
+const tela = document.getElementById('tela')
+let nome = 'Samilly'
+let idade = 14
+let maioridade = false
+
+tela.innerText = `Nome: ${nome}`
+tela.innerText += `\n Idade: ${idade}`
+tela.innerText += `\n Maioridade: ${maioridade}`
+console.log('Olá, mundo!')
