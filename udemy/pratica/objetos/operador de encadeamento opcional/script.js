4e4ed8203a7f7cc745e1a02040632ddc1a6fac05
Marcos Costa Pinheiro <marquinhos_pinheiro14@outlook.com>
2023-04-04 17:14:18 -0300

    operador de encadeamento opcional

---
+++ udemy/pratica/objetos/operador de encadeamento opcional/script.js
@@ -0,0 +1,17 @@
+function createPerson(id){
+    if(id <= 0){
+        return null
+    }   
+
+    return {
+        id: id,
+        profile: {
+            username: 'admin',
+            avatar: 'avatar.png'
+        }
+    }
+}
+
+let person = createPerson(-1)
+let profile = person ?. profile
+console.log(profile)
