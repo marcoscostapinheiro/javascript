51fcb8de0eb2c99a21e96a5e49c6ba29997f96f3
Marcos Costa Pinheiro <marquinhos_pinheiro14@outlook.com>
2023-04-05 22:01:43 -0300

    exercicio 5 aprimorado

--- udemy/exercicios/ex005/script.js
+++ udemy/exercicios/ex005/script.js
@@ -1,29 +1,34 @@
 const resp = document.getElementById('resp')
-let peso = Number(prompt('Digite seu peso'))
-let altura = Number(prompt('Digite sua altura(em metros)'))
-var imc = peso / altura ** 2
-console.log(imc)
 
 function verifyImc(){
+    let peso = Number(prompt('Digite seu peso(em kg)'))
+    let altura = Number(prompt('Digite sua altura(em metros)'))
+    let imc = peso / altura ** 2
+    let op = document.getElementsByClassName('op')
+
+    for(let item = 0; item < 4; item++){
+        op[item].style.backgroundColor = 'rgb(219, 218, 218)'
+    }
+
+    resp.innerHTML = `<p><strong>IMC:</strong> ${imc.toFixed(2)}</p>`
+
     if(imc < 18.5){
-        resp.innerText = `Seu IMC é de ${imc.toFixed(2)}. Abaixo do peso.`
+        op[0].style.backgroundColor = 'lightgreen'
     }
 
     else if(imc < 24.9){
-        resp.innerText = `Seu IMC é de ${imc.toFixed(2)}. Peso normal.`
+        op[1].style.backgroundColor = 'lightgreen'
     }
 
     else if(imc < 29.9){
-        resp.innerText = `Seu IMC é de ${imc.toFixed(2)}. Acima do peso`
+        op[2].style.backgroundColor = 'lightgreen'
     }
 
     else if(imc < 39.9){
-        resp.innerText = `Seu IMC é de ${imc.toFixed(2)}. Você está obeso.`
+        op[3].style.backgroundColor = 'lightgreen'
     }
 
     else{
-        resp.innerText = `Seu IMC é de ${imc.toFixed(2)}. Você está com obesidade grave.`
+        op[4].style.backgroundColor = 'lightgreen'
     }
 }
-
-verifyImc()
